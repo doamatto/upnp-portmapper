886bd69a8509d00e10cef60793989d3f530d0026
doamatto <hello@doamatto.xyz>
2020-06-18 08:38:10 -0700

    Added cli app and Mono.Nat as a dependency

---
+++ upnp-portmapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace upnp_portmapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
